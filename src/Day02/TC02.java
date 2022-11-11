6478aa068966df3674960208000af6a547494bd8
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-11-10 20:53:32 -0500

    TC02

---
+++ src/Day02/TC02.java
@@ -0,0 +1,27 @@
+package Day02;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC02 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(2000);
+        driver.navigate().to("https://www.exelenter.com");
+        Thread.sleep(2000);
+        driver.navigate().to("https://www.google.com");
+        driver.navigate().back();
+        driver.navigate().refresh();
+        String currentUrl = driver.getCurrentUrl();
+        if (currentUrl.contains("exelenter")){
+            System.out.println("Test passes. Website contains Exelenter");
+        }else{
+            System.out.println("Test failed. Wrong website");
+        }
+        driver.close();
+
+    }
+}
