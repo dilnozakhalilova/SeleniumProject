7a048237bec0774978e1561a51afc24ca860a79c
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-11-22 15:22:38 -0500

    Test

---
+++ src/_11_18_2022Class/XPath.java
@@ -0,0 +1,35 @@
+package _11_18_2022Class;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XPath {
+
+        private static String url = "https://exelentersdet.com/";
+        public static void main(String[] args) throws InterruptedException {
+            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe"); // window users add '.exe' at the end
+            WebDriver driver = new ChromeDriver();
+            driver.get(url);
+
+            driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+            driver.findElement(By.xpath("//input[@name='txtPassword']")).sendKeys("Exelent2022Sdet!");
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//input[@value='LOGIN']")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//a[contains(text(),'Welcome')]")).click();
+
+            Thread.sleep(2000);
+            //driver.findElement(By.xpath("//div[@id='welcome-menu']//a")).click();
+
+            driver.findElement(By.xpath("//a[starts-with(text(),'Log')]")).click();
+
+            
+            Thread.sleep(2000);
+            driver.close();
+        }
+
+
+
+    }
+
