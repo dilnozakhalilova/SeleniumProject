4399d064e4058efd6a1ead96ffef57edb03419d5
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-12-13 05:59:15 -0500

    Write to Excel

---
+++ src/Class11/HW_TC1_USA_Locator.java
@@ -0,0 +1,36 @@
+package Class11;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+import static utils.BaseClass.*;
+
+public class HW_TC1_USA_Locator {
+    public static void main(String[] args) throws InterruptedException{
+
+        setUp();
+
+        WebElement countryDD = driver.findElement(By.cssSelector("select[name='country']"));
+        Select select = new Select(countryDD);
+
+        Thread.sleep(1000);
+        select.selectByVisibleText("United States of America");
+        System.out.println("select = " + select);
+
+
+        List<WebElement> countries= driver.findElements(By.xpath("//*[@name='country']/options"));
+        for (WebElement country : countries) {
+            if(country.getText().equals("United States of America")){
+                System.out.println("country = " + country);
+                break;
+            }
+        }
+
+
+
+        tearDown();
+    }
+}
