fd76e13eb906dc0b246ba1d17557d29295573966
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-12-14 22:05:35 -0500

    Update2

--- src/Class11/DropdownDDMMYYYY.java
+++ src/Class11/DropdownDDMMYYYY.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 import static utils.BaseClass.*;
 
 public class DropdownDDMMYYYY {
@@ -26,7 +28,21 @@ public class DropdownDDMMYYYY {
         Select selectYearDD = new Select(yearDD);
         selectYearDD.selectByValue("1985");
 
+
         // NOTE: NEVER TYPE ANY LOCATORS, such as XPATH,CSS. ALWAYS COPY-PASTE.
+        Thread.sleep(2000);
+        // Loop through the dayDD, fimd a specific date (23) select and print into the console
+        List<WebElement> dayList = selectDayDD.getOptions();
+     for(WebElement day : dayList){
+         day.click();
+         String dayText = day.getText();
+         if(dayText.equals("23")){
+             System.out.println("dayText = " + dayText);
+             break;
+
+         }
+
+     }
 
         tearDown();
 
---
+++ src/Class11/DropdownDDMMYYYY_Refactor2.java
@@ -0,0 +1,82 @@
+package Class11;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+import static utils.BaseClass.*;
+
+public class DropdownDDMMYYYY_Refactor2 {
+
+    public static void main(String[] args) throws InterruptedException {
+    setUp();
+    findDay("20");
+    findMonth(5);
+    fingYear("1995");
+    selectDay("25");
+    tearDown();
+}
+    /**
+     * @param day String
+     * @return day
+     */
+    static String findDay(String day) {
+        WebElement dayDD = driver.findElement(By.xpath("(//select[@class='day '])[1]"));
+        Select selectDayDD = new Select(dayDD);
+        selectDayDD.selectByVisibleText(day);
+        System.out.println("day = " + day);
+        return day;
+    }
+
+    /**
+     * @param month int (index of given month)
+     * @return month
+     */
+    static int findMonth(int month) {
+        WebElement monthDD = driver.findElement(By.xpath("(//select[@class='month '])[1]"));
+        Select selectMonthDD = new Select(monthDD);
+        selectMonthDD.selectByIndex(month);// April
+        System.out.println("month = " + month);
+        return month;
+
+    }
+
+    /**
+     * This method accepts year as string and returns given year
+     *
+     * @param year String
+     * @return year
+     */
+    static String fingYear(String year) {
+        WebElement yearDD = driver.findElement(By.xpath("(//select[@class='year '])[1]"));
+        Select selectYearDD = new Select(yearDD);
+        selectYearDD.selectByValue(year);
+        System.out.println("year = " + year);
+        return year;
+
+    }
+
+    /**
+     *
+     * @param pickDay String
+     * @return
+     */
+    static String selectDay(String pickDay) {
+        WebElement dayDD = driver.findElement(By.xpath("(//select[@class='day '])[1]"));
+        Select selectDayDD = new Select(dayDD);
+        List<WebElement> dayList = selectDayDD.getOptions();
+        for (WebElement day : dayList) {
+            day.click();
+            String dayText = day.getText();
+            if (dayText.equals(pickDay)) {
+                System.out.println("dayText = " + dayText);
+                break;
+            }
+        }
+        return pickDay;
+    }
+
+
+}
