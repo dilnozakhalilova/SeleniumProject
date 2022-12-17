5206aaea412734d81c9636d43e486735d92a4654
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-12-16 20:42:23 -0500

    Alerts

--- src/Class12/_01_AlertDemo.java
+++ src/Class12/_01_AlertDemo.java
@@ -22,7 +22,31 @@ public class _01_AlertDemo {
         Alert alert = driver.switchTo().alert();
         String alertText =alert.getText();
         System.out.println("alertText = " + alertText);
-        alert.accept();
+        alert.accept();                                     // accept = "OK" Button
+        Thread.sleep(2000);
+
+        // Handling Confirmation Alert ===================//button[text()='Confirmation Alert']
+
+        Thread.sleep(2000);
+         driver.findElement(By.cssSelector("button#confirm")).click();
+        Alert confirmationAlert = driver.switchTo().alert();  //NOTE: for all alert types we use the same .switchTo().alert();
+        String confirmationAlertText = confirmationAlert.getText();
+        System.out.println("confirmationAlertText: " + confirmationAlertText);
+        Thread.sleep(2000);
+        confirmationAlert.dismiss();                        // dismiss="Cancel" button
+
+        // Handling Prompt Alert     =======================//button#prompt
+        Thread.sleep(2000);
+        driver.findElement(By.id("prompt")).click();        // Identify WebElement for the alert and click on it
+        Alert promptAlert = driver.switchTo().alert();      // Switching to Alert (from the browser) to handle it
+        String promptAlertText = promptAlert.getText();     // Getting the content/text of the Alert(not the browser)
+        System.out.println("promptAlertText: " + promptAlertText);
+        Thread.sleep(1000);                             // Add time only when needed( for visualization) optional
+        promptAlert.sendKeys("Dilnoza");           // Sending text/value(typing) the input text box
+        Thread.sleep(1000);
+        promptAlert.accept();                               // Press "OK" button to close the Alert
+
+
 
 
         tearDown();
