25c129ebf373a96ade5ee04596a2a6eb92f2038b
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-12-18 19:38:53 -0500

    Modal SampleRun

--- src/Class12/_04_HW_Modal.java
+++ src/Class12/_04_HW_Modal.java
@@ -1,5 +1,26 @@
 package Class12;
 
-public class _04_HW_Modal {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 
+import static utils.BaseClass.*;
+
+public class _04_HW_Modal {
+    public static void main(String[] args) throws InterruptedException {
+        setUp();
+
+        System.out.println(driver.findElement(By.id("showLargeModal")).getText());
+
+        driver.findElement(By.id("showLargeModal")).click();
+
+        //driver.findElement(By.xpath("//button[@id='showLargeModal']")).click();// By x.path--> second option
+
+        System.out.println(driver.findElement(By.xpath("//div[@class='modal-title h4']")).getText());
+
+        System.out.println(driver.findElement(By.xpath("//div[@class='modal-body']")).getText());
+
+        driver.findElement(By.xpath("//button[@id='closeLargeModal']")).click();
+
+        tearDown();
+    }
 }
