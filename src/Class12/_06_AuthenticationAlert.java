30f2f6d25e8d94fae9c7ae4ae222368eea40bf9b
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-12-19 22:40:07 -0500

    Updates

--- src/Class12/_06_AuthenticationAlert.java
+++ src/Class12/_06_AuthenticationAlert.java
@@ -4,8 +4,17 @@ import static utils.BaseClass.*;
 
 public class _06_AuthenticationAlert {
     public static void main(String[] args) {
-        setUp();
-
+        setUp();//url=https://foo:bar@httpbin.org/basic-auth/foo/bar
+/**
+ * 1. JS Alert Types
+ * 2. Modal (HTML Based) popup types
+ * 3. Basic Authentication Alert Type
+ * 4. Window based popups (not supported by Selenium or other automation programs)
+ * For the above website
+ * username= foo
+ * pw=bar
+ * url=https://foo:bar@httpbin.org/basic-auth/foo/bar
+ */
 
 
 
---
+++ src/Class13/_01_FramesDemo.java
@@ -0,0 +1,18 @@
+package Class13;
+
+import static utils.BaseClass.*;
+
+public class _01_FramesDemo {
+    public static void main(String[] args) {
+        setUp();
+
+
+
+
+
+
+
+        tearDown();
+    }
+
+}
