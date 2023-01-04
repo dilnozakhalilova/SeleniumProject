ab870a5ce3f3164bea51e39d25579eba2f8138b7
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-03 23:36:13 -0500

    Windowhandler Task

--- src/Class14/_04_Task.java
+++ src/Class14/_04_Task.java
@@ -1,5 +1,9 @@
 package Class14;
 
+import org.openqa.selenium.By;
+
+import java.util.Set;
+
 import static utils.BaseClass.*;
 /*
  *  Task: Go to this URL: https://the-internet.herokuapp.com/windows
@@ -10,13 +14,34 @@ import static utils.BaseClass.*;
  */
 
 public class _04_Task {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         setUp("https://the-internet.herokuapp.com/windows");
 
+        System.out.println(" Identify and print parent window ID and Title");
+        System.out.println();
+        String parentWindow = driver.getWindowHandle();
+        System.out.println("Parent Window ID:  " + parentWindow + " Title: " + driver.getTitle());
 
 
+        System.out.println("Part2 ");
+
+        for (int i = 0; i < 3; i++) {
+            driver.findElement(By.linkText("Click Here")).click();
+            Thread.sleep(1000);
+
+        }
+
+        Set<String> allWindowsOrTabs = driver.getWindowHandles();
+
+        for (String window : allWindowsOrTabs) {
+            if (!window.contains(parentWindow)) {
+                System.out.println("This is a new TabOrWindow: " + driver.switchTo().window(window).getTitle());
+                driver.get("https://facebook.com");
+                System.out.println("Name: " + driver.getTitle() + " URL: " + driver.getCurrentUrl());
 
 
+            }
+        }
         tearDown();
     }
 
