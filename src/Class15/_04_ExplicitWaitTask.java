28a04d98b9ef81081cbcfe15f4e2461012b94952
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-06 21:14:38 -0500

    ExplicitWaitTask

--- src/Class15/_04_ExplicitWaitTask.java
+++ src/Class15/_04_ExplicitWaitTask.java
@@ -3,6 +3,7 @@ package Class15;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -23,17 +24,18 @@ public class _04_ExplicitWaitTask {
  */
     public static void main(String[] args) {
         setUp("https://the-internet.herokuapp.com/dynamic_loading/2");
+      //  System.setProperty(ChromeDriverService.CHROME_DRIVER_APPEND_LOG_PROPERTY,"true"); // This line will get rid of extra error logs from the console
 
         //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));   // Implicit Wait. It is Dynamic and is prefered
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));  // Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));  // Explicit wait
 
         try {
             driver.findElement(By.cssSelector("div[id='start'] button")).click();  //div#start button
             wait.until(ExpectedConditions.textToBe(By.xpath("//div[@id='finish']/h4"),"Hello World!"));
             System.out.println(driver.findElement(By.xpath("//div[@id='finish']/h4")).getText());   // cssLocator    "div#finish h4"
 
-        } catch (NoSuchElementException e) { // Note: TimeOutException
+        } catch (NoSuchElementException e) { // Note: if you get this exception TimeOutException, replace with it or use parent "Exception"
             e.printStackTrace();
             System.out.println("Element is not found. Wait longer for the element to be visible ");
         }
---
+++ src/Class15/_05_ExplicitWait.java
@@ -0,0 +1,24 @@
+package Class15;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static utils.BaseClass.*;
+
+public class _05_ExplicitWait {
+    public static void main(String[] args) {
+        setUp("https://omayo.blogspot.com/");
+        var wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        driver.findElement(By.cssSelector("button[class='dropbtn']")).click();  // Locating dropdown button and click on it
+        wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Facebook")));  // We wait for facebook link to appear based on given condition
+        driver.findElement(By.partialLinkText("Face")).click();  // click on the facebook link and should take us to facebook homepage
+
+
+        tearDown();
+    }
+}
