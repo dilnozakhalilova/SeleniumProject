ec99ad7cdb969cbee1f7f68159f8a1976993e87d
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-09 21:05:57 -0500

    PageLoadTimeOuts

---
+++ src/Class15/_08_PageLoadTimeOut.java
@@ -0,0 +1,50 @@
+package Class15;
+
+
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class _08_PageLoadTimeOut {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+/**
+ * I am telling the browser, stop looking after given time, in this case, stop loading after 3 sec
+ *         In other words I am telling the webpage = "you got 1 sec to load OR I will trow and exception"
+ */
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(2)); //<==This is PageLoadTimeOut Wait.
+
+        Instant startTime = Instant.now();
+        System.out.println("startTime = " + startTime);
+
+        try{
+            driver.get("https://exelenter.com");
+
+        }catch (TimeoutException e){
+            e.printStackTrace();
+            System.out.println("===>TOO SLOW TO LOAD, ADD MORE TIME");
+
+        }
+
+
+        Instant endTime = Instant.now();
+        System.out.println("endTime = " + endTime);
+
+        Duration pageLoadTime = Duration.between(startTime, endTime);
+        System.out.println("pageLoadTime = " + pageLoadTime.toMillis());
+
+        System.out.println(driver.getTitle());
+
+        driver.close();
+
+
+    }
+
+
+}
--- src/Class16.java
+++
@@ -1,8 +0,0 @@
-import static utils.BaseClass.*;
-
-public class Class16 {
-    public static void main(String[] args) {
-        //setUp();
-        tearDown();
-    }
-}
