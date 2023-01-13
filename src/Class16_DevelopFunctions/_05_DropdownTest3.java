1f35e20a87a5fd249130235b2718c777b8c71b9a
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-12 20:38:25 -0500

    Using list of days

--- src/Class16_DevelopFunctions/_05_DropdownTest3.java
+++ src/Class16_DevelopFunctions/_05_DropdownTest3.java
@@ -3,11 +3,13 @@ package Class16_DevelopFunctions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 import static utils.BaseClass.*;
 import static utils.CommonMethods.*;
 
 public class _05_DropdownTest3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         setUp("https://vitalets.github.io/combodate/");
 
         WebElement dayDD = driver.findElement(By.xpath("(//select[@class='day '])[1]"));
@@ -20,7 +22,12 @@ public class _05_DropdownTest3 {
         selectDdValue(monthDD,"Apr");// We can use by index or visible text
         selectDdValue(yearDD,"2012");
 
+        Thread.sleep(2000);
 
+        // Without Class Select
+        // Lets assume dropdown does not have class tag, how would you locate and select dates fom the list?
+        List<WebElement> daysDD = driver.findElements(By.xpath("(//select[@class='day '])[1]/option"));
+        selectDdValue(daysDD,"17");
         tearDown();
     }
 }
