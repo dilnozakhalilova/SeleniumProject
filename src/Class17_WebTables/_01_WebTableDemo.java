96fbe238abc2c41ff46efaafbdb231672dda5826
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-17 20:40:24 -0500

    Table

--- src/Class17_WebTables/_01_WebTableDemo.java
+++ src/Class17_WebTables/_01_WebTableDemo.java
@@ -1,21 +1,18 @@
 package Class17_WebTables;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 import static utils.BaseClass.*;
 
 public class _01_WebTableDemo {
     public static void main(String[] args) {
         setUp("https://the-internet.herokuapp.com/tables");
 
-
-
-
-
-
-
-
-
-
-
+        List<WebElement> rows = driver.findElements(By.cssSelector("table#table1 tbody tr"));
+        System.out.println("Number of rows: " + rows.size());
 
 
 
