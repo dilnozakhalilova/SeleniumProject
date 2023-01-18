434d02a88cc13c2cfa3ee00057cd95bb97f7448b
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-17 20:44:24 -0500

    Accessing data from the table

--- src/Class17_WebTables/_01_WebTableDemo.java
+++ src/Class17_WebTables/_01_WebTableDemo.java
@@ -3,6 +3,7 @@ package Class17_WebTables;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.Iterator;
 import java.util.List;
 
 import static utils.BaseClass.*;
@@ -14,7 +15,14 @@ public class _01_WebTableDemo {
         List<WebElement> rows = driver.findElements(By.cssSelector("table#table1 tbody tr"));
         System.out.println("Number of rows: " + rows.size());
 
+        System.out.println("========Printing rows==========");
 
+        Iterator<WebElement> iterator = rows.iterator();
+        while(iterator.hasNext()){
+            WebElement row = iterator.next();
+            String rowText = row.getText();
+            System.out.println("rowText:  " + rowText);
+        }
 
         tearDown();
     }
