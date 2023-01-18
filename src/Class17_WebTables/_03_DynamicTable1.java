e464ae90054491f9f04a35b4d05583310ec7ecd0
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-17 22:41:29 -0500

    Refactoring

--- src/Class17_WebTables/_03_DynamicTable1.java
+++ src/Class17_WebTables/_03_DynamicTable1.java
@@ -3,14 +3,14 @@ package Class17_WebTables;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
 import java.util.List;
 
 import static utils.BaseClass.*;
 
 public class _03_DynamicTable1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         setUp("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Default.aspx");
-
 
         // First we need to log in
 
@@ -22,18 +22,22 @@ public class _03_DynamicTable1 {
         System.out.println("Number of rows: " + rows.size());
 
         // Task: Selecting a specific row and performing action on it, such as, edit or delete it
+        // Logic. Compare actual with expected, once match is found, click on the checkbox and then click on the delete button
+        // mini task.
+
         String expectedValue = "Charles Dodgeson";
         // By.css ==> table.SampleTable tr td:nth-child(10)
         // By.xPath===> table[@class='SampleTable']//tr/td[10]
-        WebElement actualValues= driver.findElement(By.xpath("//table[@class='SampleTable']//tr/td[2]"));
+        WebElement actualValues = driver.findElement(By.xpath("//table[@class='SampleTable']//tr/td[2]"));
+
+        for (int i = 0; i < rows.size(); i++) {
+            List<WebElement> checkboxRow = driver.findElements(By.xpath("(//table[@class='SampleTable']//tr/td/input)[" + i + "]"));
+            if (expectedValue.equals(actualValues)) {
+                checkboxRow.get(i).click();
+            }
 
 
-
-
-
-
-        // Logic. Compare actual with expected, once match is found, click on the checkbox and then click on the delete button
-        // mini task.
+        }
 
 
         tearDown();
@@ -41,5 +45,4 @@ public class _03_DynamicTable1 {
 }
 
 //        WebElement checkboxesList = driver.findElement(By.xpath("//table[@class='SampleTable']//tr/td[1]"));
-//        if(actualValues.equals(expectedValue)){
-// }
+//
