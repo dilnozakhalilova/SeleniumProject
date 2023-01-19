393c45831e6184c70533ea2c2d5dd3bf89527383
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-18 19:58:23 -0500

    Task

--- src/Class17_WebTables/_03_DynamicTable1.java
+++ src/Class17_WebTables/_03_DynamicTable1.java
@@ -20,20 +20,26 @@ public class _03_DynamicTable1 {
 
         List<WebElement> rows = driver.findElements(By.cssSelector("table[class='SampleTable'] tr")); //Xpath //table[@class="SampleTable"]//tr
         System.out.println("Number of rows: " + rows.size());
-
         // Task: Selecting a specific row and performing action on it, such as, edit or delete it
         // Logic. Compare actual with expected, once match is found, click on the checkbox and then click on the delete button
         // mini task.
         // By.css ==> table.SampleTable tr td:nth-child(10)
         // By.xPath===> table[@class='SampleTable']//tr/td[10]
+
+
         String expectedValue = "Charles Dodgeson";
-        List<WebElement> actualValues = driver.findElements(By.xpath("//table[@class='SampleTable']//tr/td[2]"));
+        List<WebElement> actualValues = driver.findElements(By.xpath("//table[@class='SampleTable']//tr/td[1]"));
         for (int i = 0; i < actualValues.size(); i++) {
             if (actualValues.get(i).getText().equals(expectedValue)) {
-                driver.findElements(By.xpath("(//table[@class='SampleTable']//tr/td/input)[" + i + "]"));
+                // driver.findElement(By.xpath("(//table[@class='SampleTable']//tr/td/input)[" + i + "]")).click();  // driver.findElement(By.cssSelector("input[type='checkbox']:nth-child(" + i + ")"))
+                driver.findElement(By.cssSelector("//table[@class='SampleTable']//tr" + i)).click();
                 Thread.sleep(1000);
+                driver.findElement(By.id("ctl00_MainContent_btnDelete")).click();//
+                Thread.sleep(1000);
+
                 break;
             }
+
 
         }
         tearDown();
