df1f39fc425dd0b891bacba7bc543fabdd4a1134
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-20 22:15:53 -0500

    finalize

--- src/Class18_Calendars/_02_ExpediaCalendar.java
+++ src/Class18_Calendars/_02_ExpediaCalendar.java
@@ -2,7 +2,7 @@ package Class18_Calendars;
 
 import static utils.BaseClass.*;
 
-public class _02_Expedia {
+public class _02_ExpediaCalendar {
     public static void main(String[] args) {
 
         setUp("https://www.expedia.com/Flights");
