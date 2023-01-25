a6fd8c5a763b3ef69060d569f219554fd6116c83
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-24 20:48:06 -0500

    Refactoring

--- src/Class18_Calendars/_02_ExpediaCalendar.java
+++ src/Class18_Calendars/_02_ExpediaCalendar.java
@@ -1,13 +1,26 @@
 package Class18_Calendars;
 
+import org.openqa.selenium.By;
+
 import static utils.BaseClass.*;
 
 public class _02_ExpediaCalendar {
     public static void main(String[] args) {
-
+       /*
+        Let's say we want to pick these dates
+        Depart: May 17th, 2023
+        Returning: July 26th, 2023
+         */
         setUp("https://www.expedia.com/Flights");
 
+        // Click on the 'Departing' button/field to pop the calendar
+        driver.findElement(By.id("d1-btn")).click();
 
-    tearDown();
+        // From the opened calendar , find Month and Year and store in a String for later use
+        String monthAndYear = driver.findElement(By.xpath("(//h2[@class='uitk-date-picker-month-name uitk-type-medium'])[1]")).getText();
+
+
+
+        tearDown();
     }
 }
