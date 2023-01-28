d8f41fe87de0bdbad7958782ca86a63726de610a
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-27 21:33:24 -0500

    JS Executor

--- src/Class19_Screenshot_Actions_JS/_07_ActionsDragAndDrop.java
+++ src/Class19_Screenshot_Actions_JS/_07_ActionsDragAndDrop.java
@@ -2,12 +2,33 @@ package Class19_Screenshot_Actions_JS;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utils.BaseClass;
 
 public class _07_ActionsDragAndDrop extends BaseClass {
     public static void main(String[] args) {
-        setUp("https://google.com/");
+        setUp("https://demoqa.com/droppable");
+        WebElement draggable = driver.findElement(By.id("draggable"));
+        WebElement droppable = driver.findElement(By.id("droppable"));
 
+        Actions actions = new Actions(driver);
+
+        // 1. Way
+
+        actions.clickAndHold(draggable).moveToElement(droppable).release().build().perform();   // works without .build() as well
+        wait(2);
+
+        driver.navigate().refresh();
+        // after this refresh above elements get detached from the DOM, you must relocate/reassign before you can use
+        wait(2);
+
+
+        // 2. Way
+        draggable = driver.findElement(By.id("draggable"));  //reassigning/reattaching in the DOM again
+        droppable = driver.findElement(By.id("droppable"));
+
+        actions.dragAndDrop(draggable, droppable).perform();
 
 
         tearDown();
---
+++ src/Class19_Screenshot_Actions_JS/_08_JSEecuterDemo.java
@@ -0,0 +1,18 @@
+package Class19_Screenshot_Actions_JS;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utils.BaseClass;
+
+public class _08_JSEecuterDemo extends BaseClass {
+    public static void main(String[] args) {
+        setUp("https://demoqa.com/droppable");
+
+
+
+
+        tearDown();
+
+    }
+}
