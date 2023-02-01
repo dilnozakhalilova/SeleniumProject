27202fb132495f462d292cd284b24b0210cd04e2
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-31 20:46:03 -0500

    PageFactory

--- src/Class20_POM/test/_01_LoginTest.java
+++ src/Class20_POM/test/_01_LoginTest.java
@@ -1,7 +1,28 @@
 package Class20_POM.test;
 
-public class _01_LoginTest {
+import Class20_POM.pages._01_LoginPageWithoutPageFactory;
+import utils.BaseClass;
+import utils.ConfigsReader;
 
+public class _01_LoginTest extends BaseClass {
+    public static void main(String[] args) {
+        setUp("https://exelentersdet.com/");
+
+
+        // Without PageFactory
+
+        var loginPage = new _01_LoginPageWithoutPageFactory();
+        sendText(loginPage.username, ConfigsReader.getProperties("username"));
+        sendText(loginPage.password, ConfigsReader.getProperties("password"));
+        clickButWaitForClickability(loginPage.loginBtn);
+
+
+
+
+
+
+        tearDown();
+    }
 
 
 
