b1bba1853e09a12b9b13e9da58a8fc31ee47127b
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-01-31 20:22:51 -0500

    New Class

---
+++ src/Class20_POM/pages/_01_LoginPageWithoutPageFactory.java
@@ -0,0 +1,17 @@
+package Class20_POM.pages;
+
+import static utils.BaseClass.*;
+
+public class _01_LoginPageWithoutPageFactory {
+    public static void main(String[] args) {
+        setUp("");
+
+
+
+
+
+
+
+       tearDown();
+    }
+}
